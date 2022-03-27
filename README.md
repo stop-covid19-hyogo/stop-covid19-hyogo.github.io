5f911a8721257e07afac7c774a782a5f750c2fa4
ohyama <yuki.ohyama@gmail.com>
2022-03-28 07:32:13 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# [Deprecated] 兵庫県 新型コロナウイルスまとめサイト
+
+[![兵庫県 新型コロナウイルスまとめサイト](https://user-images.githubusercontent.com/2575204/80415970-4c3f8480-890e-11ea-8e6f-5d6f5d8d63b4.png)](https://stop-covid19-hyogo.org/)
+
+## サイト終了のお知らせ
+
+2022年3月28日をもちまして、本サイトは終了いたしました。
+
+2020年3月15日のサイト公開からまる2年、皆さま本当にご協力ありがとうございました。
+
+まだまだ余談の許さない状況が続きますが、皆さまが健やかに過ごせることを心よりお祈りいたします。
+
+なお、今後の新型コロナウイルスの状況につきましては、兵庫県のホームページをご覧ください。
+
+## 貢献の仕方
+
+### コミュニケーションについて
+
+Slack でディスカッションを行っております。
+
+サイト制作にご協力頂ける方であれば誰でも参加可能です。[ここ](https://join.slack.com/t/stop-covid19-hyogo/shared_invite/zt-dlg7je1f-mKaKwRqadIj6l7dm7wSdKg)からご参加下さい。
+
+コミュニケーションにあたっては、Code for Japan の [行動規範](https://github.com/codeforjapan/codeofconduct) もご確認ください。
+
+### 行動原則
+
+[サイト構築にあたっての行動原則](./CODE_OF_CONDUCT.md)を御覧ください。
+
+## 東京都オリジナルから派生した他のサイト
+
+[Link先](https://github.com/tokyo-metropolitan-gov/covid19/blob/development/FORKED_SITES.md)を御覧ください。
+
+## 開発者向け情報
+
+サイト制作・開発に関する詳細は[プロジェクトへの参加方法](./CONTRIBUTING.md)をご確認下さい。  
+開発をお手伝いいただける方は、[開発者向け情報](./FOR_DEVELOPERS.md)を御覧ください。
+
+### CONTRIBUTORS.md への追加について
+
+[CONTRIBUTORS.md](./CONTRIBUTORS.md) はご協力いただいた皆様を紹介するためのファイルです。
+
+サイト制作に関わったエンジニアやデザイナーはもちろん、制作には関わらないけど貢献していただいた方々もご紹介できればと思ってます。何かしらの貢献を行った方はぜひ追記をお願いします。
+
+追加をご希望の場合は Pull Request を送っていただくか、この [Issue](https://github.com/stop-covid19-hyogo/covid19/issues/60) にコメントをお願いします。
+
+## ライセンス
+
+本ソフトウェアは、[MITライセンス](./LICENSE.txt)の元提供されています。
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const mode = process.env.NODE_ENV;
+const enabledSourceMap = mode === 'development';
+
+module.exports = {
+  entry: `./src/styles.scss`,
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+    assetModuleFilename: './images/[name]-[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: enabledSourceMap,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      grid: true,
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: 'dist',
+    },
+    open: true,
+  },
+  mode: mode,
+};
